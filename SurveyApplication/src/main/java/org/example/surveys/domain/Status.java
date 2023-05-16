3942121e60011df75afe693aed7b7162cb779f2b
Andras Baka <Andras_Baka@epam.com>
2023-05-16 22:30:20 +0200

    removing unnecessary custom logic from Status enum

--- SurveyApplication/src/main/java/org/example/surveys/domain/Status.java
+++ SurveyApplication/src/main/java/org/example/surveys/domain/Status.java
@@ -1,18 +1,8 @@
 package org.example.surveys.domain;
 
 public enum Status {
-    NOT_ASKED(1),
-    REJECTED(2),
-    FILTERED(3),
-    COMPLETED(4);
-
-    private final int key;
-
-    private Status(int key) {
-        this.key = key;
-    }
-
-    public int getKey() {
-        return key;
-    }
+    NOT_ASKED,
+    REJECTED,
+    FILTERED,
+    COMPLETED
 }
